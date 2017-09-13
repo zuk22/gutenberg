ffa97caf32f64512580118961160c81dad5c87b8
Rastislav Lamos <lamos.rasto@gmail.com>
2017-09-13 14:53:32 -0700

    Add Editable - WIP

--- blocks/library/contact-stripe/index.js
+++ blocks/library/contact-stripe/index.js
@@ -12,7 +12,8 @@ import classnames from 'classnames';
  * Internal dependencies
  */
 import './style.scss';
-import { registerBlockType } from '../../api';
+import { registerBlockType, source } from '../../api';
+import Editable from '../../editable';
 import InspectorControls from '../../inspector-controls';
 import TextControl from '../../inspector-controls/text-control';
 import ToggleControl from '../../inspector-controls/toggle-control';
@@ -20,6 +21,8 @@ import RangeControl from '../../inspector-controls/range-control';
 import BlockDescription from '../../block-description';
 import BlockControls from '../../block-controls';
 import BlockAlignmentToolbar from '../../block-alignment-toolbar';
+
+const { text } = source;
 
 registerBlockType( 'core/contact-stripe', {
 	title: __( 'Contact Stripe' ),
@@ -30,6 +33,16 @@ registerBlockType( 'core/contact-stripe', {
 
 	keywords: [ __( 'contact stripe' ) ],
 
+	attributes: {
+		address: {
+			type: 'string',
+			default: 'Write address...'
+		},
+		align: {
+			type: 'string'
+		}
+	},
+
 	getEditWrapperProps( attributes ) {
 		const { align } = attributes;
 		if ( 'left' === align || 'right' === align || 'wide' === align || 'full' === align ) {
@@ -37,42 +50,40 @@ registerBlockType( 'core/contact-stripe', {
 		}
 	},
 
-	edit() {
-		const svgIconLocation = '<use href="#icon-location" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-location"></use>';
-		const svgIconPhone = '<use href="#icon-phone" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-phone"></use>';
-		const svgIconMail = '<use href="#icon-mail" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-mail"></use>';
-		const svgIconTime = '<use href="#icon-time" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-time"></use>';
+	edit( { attributes, setAttributes, focus, setFocus } ) {
+
+		const { address } = attributes;
 
 		return (
 			<div className="contact-info-area  contact-info-footer-no-display">
 				<div className="contact-info-wrapper">
 					<span className="contact-info-address">
 						<a href="http://maps.google.com/maps?q=4600+international+gateway,+columbus,+oh+43219,+usa" target="_blank" className="customize-unpreviewable">
-							<svg className="icon icon-location" aria-hidden="true" role="img" dangerouslySetInnerHTML={ {__html: svgIconLocation } }>
-							</svg>
-							<span className="contact-info-label">4600 International Gateway, Columbus, OH 43219, USA</span>
+							<Editable
+								tagName="span"
+								className="contact-info-label"
+								placeholder={ __( 'Write address...' ) }
+								value={ address }
+								focus={ focus }
+								onFocus={ setFocus }
+								onChange={ ( value ) => setAttributes( { address: value } ) }
+							/>
 						</a>
 					</span>
 
 					<span className="contact-info-phone">
 						<a href="tel:+1 555-555-555">
-							<svg className="icon icon-phone" aria-hidden="true" role="img" dangerouslySetInnerHTML={ {__html: svgIconPhone } }>
-							</svg>
-						<span className="contact-info-label">+1 555-555-555</span>
+							<span className="contact-info-label">+1 555-555-555</span>
 						</a>
 					</span>
 
 					<span className="contact-info-email">
 						<a href="mailto:contact@mydomain.com">
-							<svg className="icon icon-mail" aria-hidden="true" role="img" dangerouslySetInnerHTML={ {__html: svgIconMail } }>
-							</svg>
 							<span className="contact-info-label">contact@mydomain.com</span>
 						</a>
 					</span>
 
 					<span className="contact-info-hours">
-						<svg className="icon icon-time" aria-hidden="true" role="img" dangerouslySetInnerHTML={ {__html: svgIconTime  } }>
-						</svg>
 						<span className="contact-info-hours-text">Mon - Fri: 8am - 6pm</span>
 					</span>
 				</div>
