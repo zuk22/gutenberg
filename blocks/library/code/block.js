9de6f69593e21ae9b0305ecb25313f0129b70350
James Johnson <james.johnson@ephox.com>
2017-10-26 13:41:35 +1000

    add/3016: autoselect the code block on focus

---
+++ blocks/library/code/block.js
@@ -0,0 +1,76 @@
+/**
+ * External dependencies
+ */
+import TextareaAutosize from 'react-autosize-textarea';
+import { isEqual } from 'lodash';
+
+/**
+ * WordPress dependencies
+ */
+import { Component, findDOMNode } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import './editor.scss';
+import InspectorControls from '../../inspector-controls';
+import BlockDescription from '../../block-description';
+
+class CodeBlock extends Component {
+	constructor() {
+		super( ...arguments );
+
+		this.bindRef = this.bindRef.bind( this );
+		this.updateFocus = this.updateFocus.bind( this );
+
+		this.textareaRef = null;
+	}
+
+	bindRef( nodeRef ) {
+		this.textareaRef = nodeRef;
+	}
+
+	updateFocus() {
+		const { focus } = this.props;
+		if ( ! this.textareaRef ) {
+			return;
+		}
+		const textarea = findDOMNode( this.textareaRef );
+		if ( focus && ! textarea.contains( document.activeElement ) ) {
+			setTimeout( () => textarea.focus(), 0 );
+		}
+	}
+
+	componentDidUpdate( prevProps ) {
+		if ( ! isEqual( this.props.focus, prevProps.focus ) ) {
+			this.updateFocus();
+		}
+	}
+
+	componentDidMount() {
+		this.updateFocus();
+	}
+
+	render() {
+		const { attributes, setAttributes, focus, className } = this.props;
+		return [
+			focus && (
+			<InspectorControls key="inspector">
+				<BlockDescription>
+					<p>{ __( 'The code block maintains spaces and tabs, great for showing code snippets.' ) }</p>
+				</BlockDescription>
+			</InspectorControls>
+			),
+			<TextareaAutosize
+				key="block"
+				className={ className }
+				value={ attributes.content }
+				onChange={ ( event ) => setAttributes( { content: event.target.value } ) }
+				placeholder={ __( 'Write code…' ) }
+				ref={ this.bindRef }
+			/>,
+		];
+	}
+}
+export default CodeBlock;
--- blocks/library/code/index.js
+++ blocks/library/code/index.js
@@ -1,9 +1,4 @@
 /**
- * External dependencies
- */
-import TextareaAutosize from 'react-autosize-textarea';
-
-/**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
@@ -11,10 +6,8 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
-import './editor.scss';
 import { registerBlockType, source, createBlock } from '../../api';
-import InspectorControls from '../../inspector-controls';
-import BlockDescription from '../../block-description';
+import CodeBlock from './block';
 
 const { prop } = source;
 
@@ -53,24 +46,7 @@ registerBlockType( 'core/code', {
 		],
 	},
 
-	edit( { attributes, setAttributes, focus, className } ) {
-		return [
-			focus && (
-				<InspectorControls key="inspector">
-					<BlockDescription>
-						<p>{ __( 'The code block maintains spaces and tabs, great for showing code snippets.' ) }</p>
-					</BlockDescription>
-				</InspectorControls>
-			),
-			<TextareaAutosize
-				key="block"
-				className={ className }
-				value={ attributes.content }
-				onChange={ ( event ) => setAttributes( { content: event.target.value } ) }
-				placeholder={ __( 'Write code…' ) }
-			/>,
-		];
-	},
+	edit: CodeBlock,
 
 	save( { attributes } ) {
 		return <pre><code>{ attributes.content }</code></pre>;
