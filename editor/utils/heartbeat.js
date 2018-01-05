66cf9f78d1a514370471806b6cdf8fe97f8d432a
Adam Silverstein <adam@10up.com>
2018-01-05 11:26:36 -0500

    Remove post locking code

--- editor/utils/heartbeat.js
+++ editor/utils/heartbeat.js
@@ -21,45 +21,6 @@ export function setupHeartbeat() {
 	} );
 
 	/**
-	 * Configure Heartbeat post locks.
-	 *
-	 * Used to lock editing of an object by only one user at a time.
-	 *
-	 * When the user does not send a heartbeat in a heartbeat-time
-	 * the user is no longer editing and another user can start editing.
-	 */
-	$document.on( 'heartbeat-send.refresh-lock', function( e, data ) {
-		const lock = jQuery( '#active_post_lock' ).val(),
-			postId = jQuery( '#post_ID' ).val(),
-			send = {};
-
-		if ( ! postId ) {
-			return;
-		}
-		send.post_id = postId;
-
-		if ( lock ) {
-			send.lock = lock;
-		}
-
-		data[ 'wp-refresh-post-lock' ] = send;
-	} );
-
-	// Refresh post locks: update the lock string or show the dialog if somebody has taken over editing.
-	$document.on( 'heartbeat-tick.refresh-lock', function( e, data ) {
-		if ( data[ 'wp-refresh-post-lock' ] ) {
-			const received = data[ 'wp-refresh-post-lock' ];
-
-			if ( received.lock_error ) {
-				// @todo suspend autosaving
-				// @todo Show "editing taken over" message.
-			} else if ( received.new_lock ) {
-				jQuery( '#active_post_lock' ).val( received.new_lock );
-			}
-		}
-	} );
-
-	/**
 	 * @summary Concatenates the title, content and excerpt.
 	 *
 	 * This is used to track changes when auto-saving.
