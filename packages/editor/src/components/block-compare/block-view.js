f526bf427509e7bdabf27abc7020e0b050a8d56f
John Godley <johng75@gmail.com>
2018-07-17 09:19:06 +0100

    Add block compare component
    Shows two blocks side by side with the difference in HTML highlighted

---
+++ packages/editor/src/components/block-compare/block-view.js
@@ -0,0 +1,23 @@
+import { Button } from '@wordpress/components';
+
+const CompareBlock = ( { title, rawContent, renderedContent, action, actionText, className } ) => {
+	return (
+		<div className={ className }>
+			<h1 className="components-modal__header-heading">{ title }</h1>
+
+			<div className="editor-block-compare__html">
+				{ rawContent }
+			</div>
+
+			<div className="editor-block-compare__preview edit-post-visual-editor">
+				{ renderedContent }
+			</div>
+
+			<div className="editor-block-compare__action">
+				<Button isLarge tabindex="0" onClick={ action }>{ actionText }</Button>
+			</div>
+		</div>
+	);
+};
+
+export default CompareBlock;
