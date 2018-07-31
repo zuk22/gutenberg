f35b6da64342fef7d9ec9f75de5522ee2ee7f84f
Andrés <andres.maneiro@automattic.com>
2018-07-31 11:05:08 +0200

    Deprecate isRequestingTerms and isRequestingCategories selectors

--- packages/core-data/src/selectors.js
+++ packages/core-data/src/selectors.js
@@ -71,6 +71,11 @@ export function getCategories( state ) {
  * @return {boolean} Whether a request is in progress for taxonomy's terms.
  */
 export function isRequestingTerms( state, taxonomy ) {
+	deprecated( 'isRequestingTerms selector', {
+		version: '3.6.0',
+		alternative: 'getEntitiesByKind selector',
+		plugin: 'Gutenberg',
+	} );
 	return isResolving( 'getTerms', taxonomy );
 }
 
@@ -83,6 +88,11 @@ export function isRequestingTerms( state, taxonomy ) {
  * @return {boolean} Whether a request is in progress for categories.
  */
 export function isRequestingCategories() {
+	deprecated( 'isRequestingCategories selector', {
+		version: '3.6.0',
+		alternative: 'getEntitiesByKind selector',
+		plugin: 'Gutenberg',
+	} );
 	return isResolving( 'getCategories' );
 }
 
